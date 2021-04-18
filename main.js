e2c603f2b2389612eaf06bc17894a562d80bc6da
ThatKit <emper137137@gmail.com>
2021-04-18 16:11:38 +1000

    main.js variables assignment

---
+++ main.js
@@ -0,0 +1,21 @@
+// Storing references to game boxes in vars
+
+const leftTop = document.querySelector('.left.top');
+const leftMid = document.querySelector('.left.mid');
+const leftBot = document.querySelector('.left.bot');
+
+const centerTop = document.querySelector('.center.top');
+const centerMid = document.querySelector('.center.mid');
+const centerBot = document.querySelector('.center.bot');
+
+const rightTop = document.querySelector('.right.top');
+const rightMid = document.querySelector('.right.mid');
+const rightBot = document.querySelector('.right.bot');
+
+// Storing game values of 'X' and 'O' in vars
+
+const x = 'X';
+const o = 'O';
+
+// User-end interactions
+
