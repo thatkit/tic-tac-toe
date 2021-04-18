70650f100dd20e8790afa39d0f7f86088cb96f5a
ThatKit <emper137137@gmail.com>
2021-04-19 07:57:35 +1000

    computer can mark now

--- main.js
+++ main.js
@@ -7,14 +7,14 @@
 const leftTop = document.querySelector('.left.top');
 const leftMid = document.querySelector('.left.mid');
 const leftBot = document.querySelector('.left.bot');
-
 const centerTop = document.querySelector('.center.top');
 const centerMid = document.querySelector('.center.mid');
 const centerBot = document.querySelector('.center.bot');
-
 const rightTop = document.querySelector('.right.top');
 const rightMid = document.querySelector('.right.mid');
 const rightBot = document.querySelector('.right.bot');
+
+const arrBoxRefs = [leftTop, leftMid, leftBot, centerTop, centerMid, centerBot, rightTop, rightMid, rightBot];
 
 // Storing game values of 'X' and 'O' in vars
 
@@ -65,6 +65,8 @@ let centerBotVal = 0;
 let rightTopVal = 0;
 let rightMidVal = 0;
 let rightBotVal = 0;
+
+const arrBoxVals = [leftTopVal, leftMidVal, leftBotVal, centerTopVal, centerMidVal, centerBotVal, rightTopVal, rightMidVal, rightBotVal];
 
 //Winning combinations
 
@@ -124,6 +126,19 @@ User-end interactions
 const markX = val => val.innerHTML = `<p class='marked-x'>${x}</p>`;
 const markO = val => val.innerHTML = `<p class='marked-o'>${o}</p>`;
 
-leftTop.onclick = () => {
-    markX(leftTop);
+//Computer marking boxes
+
+const compMarks = whoStarts => {
+    let markVal = -1;
+    
+    let arrInd = Math.floor(Math.random() * 9);
+    arrBoxVals[arrInd] = markVal;
+
+    if (whoStarts === 'computer') {
+        markO(arrBoxRefs[arrInd]);
+    } else {
+        markX(arrBoxRefs[arrInd]);
+    }
+
+    turnCountIncrements();
 }
